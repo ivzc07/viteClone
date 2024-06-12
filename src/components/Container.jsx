f5a2e6ce9b1eb12d25a41815231762380edc3db8
Isaac Vazquez <ivzc07@gmail.com>
2024-06-12 14:29:21 -0600

    ADD : grid

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import Content from './Content'
+
+export default function Container(){
+    return(
+        <div className="container">
+            <div className='main-container'> 
+                <Content />
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Content.jsx
@@ -0,0 +1,27 @@
+
+export default function Content (){
+    return(
+        <div id='content-container'>
+            <div id='grid-container'>
+                <div id='right-side-content'>
+                    <p id='text-right-side-content'>
+                        <span id='title'>Vite</span>
+                        <br></br>
+                        Next Generation
+                        Frontend Tooling
+                        
+                    </p>
+                    <p id='paragraph-right-side-content'>Get ready for a development environment that can finally catch up with you.</p>
+                    
+                </div>
+                <div id='rigth-side-content-buttons'>
+                    <div className="">
+
+                    </div>
+                </div>
+                <div id='left-side-content'>
+                </div>   
+            </div>
+        </div>
+    )
+}
