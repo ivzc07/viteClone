5fc5b6c908393b65b93ae3333b5e58625e002c1d
Isaac Vazquez <ivzc07@gmail.com>
2024-06-12 19:02:30 -0600

    Content update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,23 +3,26 @@ export default function Content (){
     return(
         <div id='content-container'>
             <div id='grid-container'>
-                <div id='right-side-content'>
-                    <p id='text-right-side-content'>
-                        <span id='title'>Vite</span>
-                        <br></br>
-                        Next Generation
-                        Frontend Tooling
+                <div id='right-side-content-area'>
+                    <div id='grid-right-side-content'>
+                        <p id='text-right-side-content'>
+                            <span id='title'>Vite</span>
+                            <br></br>
+                            Next Generation
+                            Frontend Tooling
+                            <p id='paragraph-right-side-content'>
+                                Get ready for a development environment that can finally catch up with you.</p>
+                        </p>
+                        <div className="buttons-right-side-content">
+                            <anchor className="button-style">Get Started</anchor>
                         
-                    </p>
-                    <p id='paragraph-right-side-content'>Get ready for a development environment that can finally catch up with you.</p>
+                        </div>
+                    </div>
+    
                     
                 </div>
-                <div id='rigth-side-content-buttons'>
-                    <div className="">
-
-                    </div>
-                </div>
                 <div id='left-side-content'>
+                    <img src="https://vitejs.dev/logo-with-shadow.png" />
                 </div>   
             </div>
         </div>
